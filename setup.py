1cdf97d1a7e0ec0e1de5d630ee78f5f8d76a9d36
Masudur Rahman <masudaucb1303@gmail.com>
2026-07-07 21:44:27 +0600

    setup completed

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name = "src",
+    name = "vehicle_insurance_mlops",
     version = "0.0.1",
     author = "Masudur Rahman",
     author_email = "masudaucb1303@gmail.com",
