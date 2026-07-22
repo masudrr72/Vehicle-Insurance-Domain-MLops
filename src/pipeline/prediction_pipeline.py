669f01463bc3339c9f8ad48d708490a09a1c9a9d
Masudur Rahman <masudaucb1303@gmail.com>
2026-07-22 17:07:48 +0600

    feat: FASTAPI api created

--- src/pipeline/prediction_pipeline.py
+++ src/pipeline/prediction_pipeline.py
@@ -1,11 +1,68 @@
+import sys
+import pandas as pd
+from pandas import DataFrame
+
+from src.exception import MyException
+from src.logger import logging
 from pandas import DataFrame
 
 from src.entity.azure_estimator import AzureEstimator
 from src.entity.config_entity import VehiclePredictorConfig
-from src.exception import MyException
-from src.logger import logging
 
-import sys
+
+class VehicleData:
+    """
+    Represents a single customer input for prediction.
+    """
+
+    def __init__(
+        self,
+        Gender,
+        Age,
+        Driving_License,
+        Region_Code,
+        Previously_Insured,
+        Vehicle_Age,
+        Vehicle_Damage,
+        Annual_Premium,
+        Policy_Sales_Channel,
+        Vintage,
+    ):
+
+        try: 
+            self.Gender = Gender
+            self.Age = Age
+            self.Driving_License = Driving_License
+            self.Region_Code = Region_Code
+            self.Previously_Insured = Previously_Insured
+            self.Vehicle_Age = Vehicle_Age
+            self.Vehicle_Damage = Vehicle_Damage
+            self.Annual_Premium = Annual_Premium
+            self.Policy_Sales_Channel = Policy_Sales_Channel
+            self.Vintage = Vintage
+
+        except Exception as e:
+            raise MyException(e, sys) from e
+        
+
+    def get_data_as_dataframe(self)-> DataFrame:
+
+        data = {
+            "Gender": [self.Gender],
+            "Age": [self.Age],
+            "Driving_License": [self.Driving_License],
+            "Region_Code": [self.Region_Code],
+            "Previously_Insured": [self.Previously_Insured],
+            "Vehicle_Age": [self.Vehicle_Age],
+            "Vehicle_Damage": [self.Vehicle_Damage],
+            "Annual_Premium": [self.Annual_Premium],
+            "Policy_Sales_Channel": [self.Policy_Sales_Channel],
+            "Vintage": [self.Vintage],
+        }
+
+        return pd.DataFrame(data)
+    
+
 
 
 class PredictionPipeline:
@@ -60,3 +117,4 @@ class PredictionPipeline:
 
         except Exception as e:
             raise MyException(e, sys) from e
+    
